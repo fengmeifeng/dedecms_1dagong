9672864d023310d21a762b93cb275160698c609e
fengmeifeng <761450221@qq.com>
2019-11-18 16:09:09 +0800

    提交

---
+++ gongzuoku/jigong/chanche_chachegong/0215G32015.php
@@ -0,0 +1,431 @@
+<!DOCTYPE html>
+<html lang="zh-cn">
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="Cache-Control" content="no-transform" />
+	<meta http-equiv="Cache-Control" content="no-siteapp" />
+	<meta name="keywords" content="" />
+	<meta name="description" content="" />
+	<title>理货员、叉车司机_壹打工网-工作让生活更精彩</title>
+	<!-- head_css-->
+	<link href="/files/style/css/bootstrap.css" type="text/css" rel="stylesheet" /> 
+
+	<link href="/files/style/css/all.css" type="text/css" rel="stylesheet" />
+
+	<script src="/files/style/js/jquery.min.js" language="JavaScript" type="text/javascript" ></script>
+	<script src="/files/style/js/bootstrap.min.js" language="JavaScript" type="text/javascript" ></script>
+    <script src="/files/style/js/a_modal.js" language="JavaScript" type="text/javascript" ></script>
+	<script language="javascript" type="text/javascript" src="/include/login.js"></script>
+	<script language="javascript" type="text/javascript">
+		<!--
+			
+			function CheckLogin(){
+			  var taget_obj = document.getElementById('headlogin');
+			  myajax = new DedeAjax(taget_obj,false,false,'','','');
+			  myajax.SendGet2("/home/headlogin.php");
+			  DedeXHTTP = null;
+			}
+		-->
+		<!--
+			function Checkbaoming(){
+				var checkbaoming= document.getElementById('checkbaoming');
+				var aid= document.getElementById('aid');
+				var mid= document.getElementById('mid');
+				var type= document.getElementById('type');
+				//alert(type.value);
+				myajax = new DedeAjax(checkbaoming,false,false,'','','');
+				myajax.SendGet2("/home/checkbaoming.php?aid="+aid.value+"&mid="+mid.value+"&type="+type.value);
+				DedeXHTTP = null;
+			}
+		-->
+
+	</script>
+		<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media
+		queries -->
+		<!--[if lt IE 9]>
+			<script src="/files/js/html5shiv.min.js">
+			</script>
+			<script src="/files/js/respond.min.js">
+			</script>
+		<![endif]-->
+	<!--head_css end -->	
+</head>
+<body>
+<!-- head -->
+
+<div class=" beijing">
+<div class="container" >
+<div class="top_sh row">
+
+     <div class="col-xs-12 col-md-6 mp">
+        <ul class="login jidns pull-left">
+       	  <li class="pr_10 hotline dropwnd"><a data-trigger="modal" href="http://www.1dagong.com/ajax/number.html" data-title="Modal title">免费咨询</a></li>
+          <li class="phone pr_10"><a data-trigger="modal" href="http://www.1dagong.com/ajax/qrcode.html" data-title="Modal title">手机壹打工</a></li>
+        </ul>
+     </div>
+     
+     <div class="col-xs-12 col-md-6 mp">
+     	<div class="free">
+        全国免费求职热线： 
+        <a href="tel:400-118-5188">400-118-5188</a>
+        </div>
+     </div>
+</div>    
+</div>
+</div>
+
+
+<nav class="navbar navbar-default top">
+
+  <div class="container">
+    
+    <div class="navbar-header col-xs-5 col-md-2">
+      <a href="/" class="logo"><img src="/files/style/image/logo.png" height="90" width="190" class="img-responsive hidden-xs" />
+      <img src="/files/style/image/logo.jpg" class="img-responsive visible-xs sjlogo" /></a>
+    </div>
+    
+    
+    <div  id="headlogin" class="col-md-3 logint pull-right mp">
+     
+        <ul class="login pull-right bcsa">
+          <li class="pr_10 hogin dropwnd"><a href="/home/"  data-target="#denglu">登录</a></li>
+          <li><a href="/home/p_register.php" class="#denglu">注册 </a></li>
+        </ul>
+		<script language="javascript" type="text/javascript">CheckLogin();</script>
+    </div>
+    
+    
+    <div class="navbar-header pull-left" >
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse col-md-7" id="bs-example-navbar-collapse-1">
+	<script>
+	$(document).ready(function() {
+        
+        //放置特效css cclassName
+        var navUlliahover = "navUlliahover";
+        //获取当前导航的位置
+        var curl = window.location.href;
+        var chost = window.location.host;
+        var pageUrl = curl.substr(curl.lastIndexOf(chost) + chost.length);
+        //导航栏滑动事件
+        $(".navUl li a").hover(function() {
+            this.className = navUlliahover;
+            },
+            function() {
+                //当前url与导航url相匹配时，不清除特效
+                if (pageUrl != $(this).attr("href")) {
+                    this.className = "";
+                }
+            });
+        //导航列表状态
+        $(".navUl li a").each(function() {
+            if ($(this).attr("href") == pageUrl) {
+                this.className = navUlliahover;
+            }
+        });
+    });
+//-------------------------------------------------------------------------------------
+	$(document).ready(function(){  
+		$('.nav li a').each(function(){  
+			if($($(this))[0].href==String(window.location))  
+				$(this).parent().addClass('active');  
+		});  
+	})
+	</script>
+      <ul class="nav navbar-nav ">
+      	<li><a href="/">首页</a></li>
+        <li><a href="/plus/search.php?typeid=41&starttime=-1&areacode=-1&searchtype=-1">找工作</a></li>
+        <li><a href="/gongzuo/">行业名企</a></li>
+      </ul>
+    
+    </div><!-- /.navbar-collapse -->
+    
+
+  </div><!-- /.container-fluid -->
+</nav>
+
+<!--head end -->
+
+
+
+<!--            列表                  -->
+
+<div class="container">
+    <ol class="breadcrumb "><!--mt_20-->
+		<li class="active"><a href="/">网站首页</a></li>
+        <li><a href="/plus/search.php?typeid=41&starttime=-1&areacode=-1&searchtype=-1">找工作</a></li>
+        <li><a href="" />铲车/叉车工</a></li>
+		<li>理货员、叉车司机</li>
+    </ol>
+</div>
+    
+
+<div class="container">
+	<div class="row">
+    	<div class="col-md-9">
+        	<div class="find mr_20">
+                
+                <div class="l_isj">
+                	<h1>理货员、叉车司机</h1>
+                </div>
+                <div class="h1_box">发布时间： 2015年02月15日</div>
+                <div class="over">
+                    <ul class="a_box">
+                        <li ><span>工资：</span><strong>2000-3000</strong></font><span>元/月</span></li>
+                        <li ><span>公司：</span><a href="/home/index.php?uid=1&action=archives&channelid=81">宝供物流企业集团</a></li>
+                        <li ><span>地区：</span>安徽省 - 合肥市</li><!--{field:nativeplace function="Getsysenum(@me)"/}-->
+                        <li ><span>性别要求：</span>不限</li>
+                        <li ><span>学历要求：</span>高中</li>
+                        <li ><span>招聘人数：</span>20人人</li>
+                        <!--<li ><span>报名人数：</span></li>-->
+                        <li ><span>截止时间：</span>1970-01-01</li>
+                     </ul>
+                </div>
+                <div class="delivery text-center"><a class="btn btn-warning" href="/plus/toudi.php?aid=713&job=1&c=宝供物流企业集团"> 投递简历 </a>
+                <a class="btn btn-default ml_30" href="/home/pt_gongzuo.php?channelid=81"> 我也要发布职位 </a></div>
+                <div class="d_led zhiwei-ms">
+                	<h4>职位描述</h4>
+                    
+                    <dl>
+                        宝供物流企业集团有限公司
+公司简介：
+宝供物流企业集团有限公司（P.G.LOGISTICS GROUP CO., LTD）创建于1994年，总部设在广州，是国内第一家经国家工商总局批准以物流名称注册的企业集团，是中国最早运用现代物流理念为客户提供物流一体化服务的专业公司，也是目前中国领先的第三方物流企业。
+2002年12月，宝供物流被中国物流与采购联合会命名为“中国物流示范基地”，成为当年入选的唯一一家第三方物流企业。迄今为止，宝供集团多次被国家级专业协会评定为“中国物流百强企业”及“中国5A级物流企业”，并连续6年入选“中国企业信息化500强”。2010年，公司入选“中国物流品牌价值百强企业”，“宝供”商标被国家工商行政管理总局商标局正式认定为“中国驰名商标”，是第三方综合物流业唯一获得驰名商标的企业。2010年被认定为国家高新技术企业。 
+招聘岗位：
+1、	仓库调度：中专及以上学历，1年以上第三方物流相关岗位工作经验，薪资面议；
+2、	车间主任：中专及以上学历，1年以上相关岗位工作经验，薪资面议；
+3、	文员：大专及以上学历，沟通能力强，熟练掌握常用办公软件，薪资面议；
+4、	仓管员： 18-35岁，中专及以上学历，物流管理专业优先， 2400-3000元/月；
+5、	理货员：18-35岁，中专及以上学历，物流管理专业优先，2500-3200元/月；
+6、	叉车司机：18-45岁，初中及以上学历，有叉车证，2500-4000元/月；
+7、	现场安全员： 25-55岁，中专及以上学历，薪资面议；
+
+福利：
+1、购买五项社会保险和商业保险；
+2、免费提供住宿；
+3、提供餐费补贴、夜宵；
+4、享受带薪年休假；
+5、传统节日发放福利；
+6、超额奖励、年终奖；
+7、每年组织一次旅游；
+公交路线：自明珠广场坐601路公交车到方莲路口站台下，向前走100米到十字路口后向右转前行500米看见蓝白色厂房即到。
+联系电话：卢先生 13955153324
+                    </dl>
+                    
+                    <div class="write">
+                    	<div class="pull-left resume">在壹打工网<a href="/home/pt_jianli.php">完善简历</a>，即有机会被企业和猎头推荐应聘</div>
+                    	<div class="delivery pull-right"> <a class="btn btn-primary" href="/plus/toudi.php?aid=713&job=1&c=宝供物流企业集团" style="color:#fff;">投递简历</a></div>
+                    </div>
+                </div>
+                
+            </div> 
+            
+        </div>
+        
+        <div class="col-md-3 col-sm-12 rcon">
+            
+            <div class="mbott mb_30 mr_10">
+                <a href="#">
+                    <img class="img-responsive" title="1+2事业平台" src="/files/style/image/nei_12.jpg" data-bd-imgshare-binded="1">
+                </a>
+            </div>
+            <ul class="list-group mr_10">
+                <li class="list-group-item">相关的招聘企业</li>
+            </ul>
+			<dl class="mr_10">
+             <dt><a href='/plus/view.php?aid=1031'>55555555</a></dt>
+					<dd>暂无职位描述</dd>
+						<dd class="bb" style="height:20px;">
+					<!-- <dd class='bb'><b>没有人</b>报名 --><u><a href="/plus/view.php?aid=1031">我想去</a></u></dd>
+<dt><a href='/plus/view.php?aid=714'>叉车司机/驻外叉车司机</a></dt>
+					<dd>叉车司机/驻外叉车司机（男性，年龄18-50周岁）1、中专及以上学历，须持有叉...</dd>
+						<dd class="bb" style="height:20px;">
+					<!-- <dd class='bb'><b>没有人</b>报名 --><u><a href="/plus/view.php?aid=714">我想去</a></u></dd>
+<dt><a href='/plus/view.php?aid=713'>理货员、叉车司机</a></dt>
+					<dd>宝供物流企业集团有限公司
+公司简介：
+宝供物流企业集团有限公司（P.G.LOG...</dd>
+						<dd class="bb" style="height:20px;">
+					<!-- <dd class='bb'><b>没有人</b>报名 --><u><a href="/plus/view.php?aid=713">我想去</a></u></dd>
+<dt><a href='/plus/view.php?aid=712'>叉车工/拖车工</a></dt>
+					<dd>1、男性、身体健康、年龄18岁至50岁，一年以上工作经验，持有叉车证优先。...</dd>
+						<dd class="bb" style="height:20px;">
+					<!-- <dd class='bb'><b>没有人</b>报名 --><u><a href="/plus/view.php?aid=712">我想去</a></u></dd>
+<dt><a href='/plus/view.php?aid=711'>普工/叉车工</a></dt>
+					<dd>岗位：普工（叉车工） 人数不限
+要求：男女不限，年龄18岁-35岁，初中以上学...</dd>
+						<dd class="bb" style="height:20px;">
+					<!-- <dd class='bb'><b>没有人</b>报名 --><u><a href="/plus/view.php?aid=711">我想去</a></u></dd>
+
+			</dl> 
+            </div>            
+            
+        </div>
+        
+        
+    </div>
+</div>
+    
+<!--            列表                  -->    
+
+
+
+<!-------baoming------->   
+<div class="extra">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-6 col-sm-3">
+				<h5>
+					用户帮助
+				</h5>
+				<ul class="icons-list">
+					
+					<li>
+						<i class="icon-li fa fa-angle-double-right">
+						</i>
+						<a href="#">
+							帐户状态
+						</a>
+					</li>
+					<li>
+						<i class="icon-li fa fa-angle-double-right">
+						</i>
+						<a href="#">
+							密码找回与修改
+						</a>
+					</li>
+				</ul>
+			</div>
+			<!-- /span3 -->
+			<div class="col-xs-6 col-sm-3">
+				<h5>
+					关注我们
+				</h5>
+				<ul class="icons-list">
+					<li>
+						<i class="icon-li fa fa-angle-double-right">
+						</i>
+						<a href="/home/weixin_x.php">
+							微信
+						</a>
+					</li>
+					<li>
+						<i class="icon-li fa fa-angle-double-right">
+						</i>
+						<a href="http://www.1dagong.com/m/index.html">
+							手机客户端
+						</a>
+					</li>
+					<li>
+						<i class="icon-li fa fa-angle-double-right">
+						</i>
+						<a href="http://www.1dagong.com/">
+							手机网页版
+						</a>
+					</li>
+					<li>
+						<i class="icon-li fa fa-angle-double-right">
+						</i>
+						<a href="#">
+							新浪微博
+						</a>
+					</li>
+				</ul>
+			</div>
+			<!-- /span3 -->
+			<div class="col-xs-12 col-sm-3">
+				<h5>
+					壹打工网相关
+				</h5>
+				<ul class="icons-list">
+					<li>
+						<i class="icon-li fa fa-twitter">
+						</i>
+						<a href="#" target="_blank">
+							友情链接
+						</a>
+					</li>
+				</ul>
+			</div>
+			<!-- /span3 -->
+			<div class="col-xs-12 col-sm-3">
+				<h5>
+					免费报名
+				</h5>
+				<p>
+					仅需留下您的手机或电话，我们专业的求职顾问便会迅速为您量身定制合适的职位。
+				</p>
+				<div id="mc_embed_signup">
+					<form class="form-signin" role="form" method="post" action="/home/p_register.php" id="regUser">
+						<input type="hidden" value="regbase" name="dopost"/>
+						<input type="hidden" value="1" name="step"/>
+						<input type="hidden" value="个人" name="mtype"/>
+
+						<div class="mc-field-group form-group">
+							<input type="text" class="required email form-control" placeholder="手机号码" required name="shouye" id="userid" maxlength="13">
+							<datalist id="userid">
+								<option value="0551-12345678">
+								<option value="13912345678">
+								<option value="055112345678">
+							</datalist>
+
+						</div>
+						
+						<div class="clear">
+							<button class="btn btn-primary btn-block" type="submit">免费报名</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+		<!-- /row -->
+	</div>
+	<!-- /container -->
+</div>
+<!--------baoming------->
+
+
+<!-- /footer -->
+<div class="footer">
+	<div class="container">
+		<ul class="nav footer-nav pull-left">
+			<li>
+				© 2013-2015 Www.1dagong.Com 本站所有招聘信息及作品，未经书面授权不得转载！<br />
+				<a href="/help/aboutus.html">关于我们</a>
+			</li>
+		</ul>
+		<ul class="nav footer-nav pull-right">
+			<li>
+				最专业的求职招聘网站，致力打造人才网第一品牌！
+				<i class="fa fa-heart text-primary">
+				</i>
+				&amp;
+				<a href="http://www.1dagong.com" target="_blank">
+					壹打工网.
+				</a><br />
+				国家工业和信息化部备案号：<a href="http://www.miibeian.gov.cn/">皖ICP备13015030号-3</a>
+			</li>
+		</ul>
+	</div>
+	<!-- /container -->
+</div>
+<div class="none">
+<script type="text/javascript">
+var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3Fb41da31689d0e576af45aaeafc3be8d8' type='text/javascript'%3E%3C/script%3E"));
+</script>
+</div>
+<!-- /footer -->
+
+
+</body>
+</html>
---
+++ gongzuoku/jigong/diangong/0215J52015.php
@@ -0,0 +1,401 @@
+<!DOCTYPE html>
+<html lang="zh-cn">
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="Cache-Control" content="no-transform" />
+	<meta http-equiv="Cache-Control" content="no-siteapp" />
+	<meta name="keywords" content="" />
+	<meta name="description" content="" />
+	<title>急聘电工_壹打工网-工作让生活更精彩</title>
+	<!-- head_css-->
+	<link href="/files/style/css/bootstrap.css" type="text/css" rel="stylesheet" /> 
+
+	<link href="/files/style/css/all.css" type="text/css" rel="stylesheet" />
+
+	<script src="/files/style/js/jquery.min.js" language="JavaScript" type="text/javascript" ></script>
+	<script src="/files/style/js/bootstrap.min.js" language="JavaScript" type="text/javascript" ></script>
+    <script src="/files/style/js/a_modal.js" language="JavaScript" type="text/javascript" ></script>
+	<script language="javascript" type="text/javascript" src="/include/login.js"></script>
+	<script language="javascript" type="text/javascript">
+		<!--
+			
+			function CheckLogin(){
+			  var taget_obj = document.getElementById('headlogin');
+			  myajax = new DedeAjax(taget_obj,false,false,'','','');
+			  myajax.SendGet2("/home/headlogin.php");
+			  DedeXHTTP = null;
+			}
+		-->
+		<!--
+			function Checkbaoming(){
+				var checkbaoming= document.getElementById('checkbaoming');
+				var aid= document.getElementById('aid');
+				var mid= document.getElementById('mid');
+				var type= document.getElementById('type');
+				//alert(type.value);
+				myajax = new DedeAjax(checkbaoming,false,false,'','','');
+				myajax.SendGet2("/home/checkbaoming.php?aid="+aid.value+"&mid="+mid.value+"&type="+type.value);
+				DedeXHTTP = null;
+			}
+		-->
+
+	</script>
+		<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media
+		queries -->
+		<!--[if lt IE 9]>
+			<script src="/files/js/html5shiv.min.js">
+			</script>
+			<script src="/files/js/respond.min.js">
+			</script>
+		<![endif]-->
+	<!--head_css end -->	
+</head>
+<body>
+<!-- head -->
+
+<div class=" beijing">
+<div class="container" >
+<div class="top_sh row">
+
+     <div class="col-xs-12 col-md-6 mp">
+        <ul class="login jidns pull-left">
+       	  <li class="pr_10 hotline dropwnd"><a data-trigger="modal" href="http://www.1dagong.com/ajax/number.html" data-title="Modal title">免费咨询</a></li>
+          <li class="phone pr_10"><a data-trigger="modal" href="http://www.1dagong.com/ajax/qrcode.html" data-title="Modal title">手机壹打工</a></li>
+        </ul>
+     </div>
+     
+     <div class="col-xs-12 col-md-6 mp">
+     	<div class="free">
+        全国免费求职热线： 
+        <a href="tel:400-118-5188">400-118-5188</a>
+        </div>
+     </div>
+</div>    
+</div>
+</div>
+
+
+<nav class="navbar navbar-default top">
+
+  <div class="container">
+    
+    <div class="navbar-header col-xs-5 col-md-2">
+      <a href="/" class="logo"><img src="/files/style/image/logo.png" height="90" width="190" class="img-responsive hidden-xs" />
+      <img src="/files/style/image/logo.jpg" class="img-responsive visible-xs sjlogo" /></a>
+    </div>
+    
+    
+    <div  id="headlogin" class="col-md-3 logint pull-right mp">
+     
+        <ul class="login pull-right bcsa">
+          <li class="pr_10 hogin dropwnd"><a href="/home/"  data-target="#denglu">登录</a></li>
+          <li><a href="/home/p_register.php" class="#denglu">注册 </a></li>
+        </ul>
+		<script language="javascript" type="text/javascript">CheckLogin();</script>
+    </div>
+    
+    
+    <div class="navbar-header pull-left" >
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse col-md-7" id="bs-example-navbar-collapse-1">
+	<script>
+	$(document).ready(function() {
+        
+        //放置特效css cclassName
+        var navUlliahover = "navUlliahover";
+        //获取当前导航的位置
+        var curl = window.location.href;
+        var chost = window.location.host;
+        var pageUrl = curl.substr(curl.lastIndexOf(chost) + chost.length);
+        //导航栏滑动事件
+        $(".navUl li a").hover(function() {
+            this.className = navUlliahover;
+            },
+            function() {
+                //当前url与导航url相匹配时，不清除特效
+                if (pageUrl != $(this).attr("href")) {
+                    this.className = "";
+                }
+            });
+        //导航列表状态
+        $(".navUl li a").each(function() {
+            if ($(this).attr("href") == pageUrl) {
+                this.className = navUlliahover;
+            }
+        });
+    });
+//-------------------------------------------------------------------------------------
+	$(document).ready(function(){  
+		$('.nav li a').each(function(){  
+			if($($(this))[0].href==String(window.location))  
+				$(this).parent().addClass('active');  
+		});  
+	})
+	</script>
+      <ul class="nav navbar-nav ">
+      	<li><a href="/">首页</a></li>
+        <li><a href="/plus/search.php?typeid=41&starttime=-1&areacode=-1&searchtype=-1">找工作</a></li>
+        <li><a href="/gongzuo/">行业名企</a></li>
+      </ul>
+    
+    </div><!-- /.navbar-collapse -->
+    
+
+  </div><!-- /.container-fluid -->
+</nav>
+
+<!--head end -->
+
+
+
+<!--            列表                  -->
+
+<div class="container">
+    <ol class="breadcrumb "><!--mt_20-->
+		<li class="active"><a href="/">网站首页</a></li>
+        <li><a href="/plus/search.php?typeid=41&starttime=-1&areacode=-1&searchtype=-1">找工作</a></li>
+        <li><a href="" />电工</a></li>
+		<li>急聘电工</li>
+    </ol>
+</div>
+    
+
+<div class="container">
+	<div class="row">
+    	<div class="col-md-9">
+        	<div class="find mr_20">
+                
+                <div class="l_isj">
+                	<h1>急聘电工</h1>
+                </div>
+                <div class="h1_box">发布时间： 2015年02月15日</div>
+                <div class="over">
+                    <ul class="a_box">
+                        <li ><span>工资：</span><strong>5000-8000</strong></font><span>元/月</span></li>
+                        <li ><span>公司：</span><a href="/home/index.php?uid=1&action=archives&channelid=81">安徽黄氏和盛（集</a></li>
+                        <li ><span>地区：</span>安徽省 - 合肥市</li><!--{field:nativeplace function="Getsysenum(@me)"/}-->
+                        <li ><span>性别要求：</span>男</li>
+                        <li ><span>学历要求：</span>不限</li>
+                        <li ><span>招聘人数：</span>2 人人</li>
+                        <!--<li ><span>报名人数：</span></li>-->
+                        <li ><span>截止时间：</span>2016-04-06</li>
+                     </ul>
+                </div>
+                <div class="delivery text-center"><a class="btn btn-warning" href="/plus/toudi.php?aid=745&job=1&c=安徽黄氏和盛（集"> 投递简历 </a>
+                <a class="btn btn-default ml_30" href="/home/pt_gongzuo.php?channelid=81"> 我也要发布职位 </a></div>
+                <div class="d_led zhiwei-ms">
+                	<h4>职位描述</h4>
+                    
+                    <dl>
+                        一、负责公司工厂、店面强弱电的安装维修；
+二、负责机械设备的安装维修；
+三、其他领导交办事项；
+四、25周岁以上、三年以上电工维修工工作经验；
+五、持有电工证。
+                    </dl>
+                    
+                    <div class="write">
+                    	<div class="pull-left resume">在壹打工网<a href="/home/pt_jianli.php">完善简历</a>，即有机会被企业和猎头推荐应聘</div>
+                    	<div class="delivery pull-right"> <a class="btn btn-primary" href="/plus/toudi.php?aid=745&job=1&c=安徽黄氏和盛（集" style="color:#fff;">投递简历</a></div>
+                    </div>
+                </div>
+                
+            </div> 
+            
+        </div>
+        
+        <div class="col-md-3 col-sm-12 rcon">
+            
+            <div class="mbott mb_30 mr_10">
+                <a href="#">
+                    <img class="img-responsive" title="1+2事业平台" src="/files/style/image/nei_12.jpg" data-bd-imgshare-binded="1">
+                </a>
+            </div>
+            <ul class="list-group mr_10">
+                <li class="list-group-item">相关的招聘企业</li>
+            </ul>
+			<dl class="mr_10">
+             <dt><a href='/plus/view.php?aid=745'>急聘电工</a></dt>
+					<dd>一、负责公司工厂、店面强弱电的安装维修；
+二、负责机械设备的安装维修；...</dd>
+						<dd class="bb" style="height:20px;">
+					<!-- <dd class='bb'><b>没有人</b>报名 --><u><a href="/plus/view.php?aid=745">我想去</a></u></dd>
+<dt><a href='/plus/view.php?aid=746'>水电工</a></dt>
+					<dd>任职要求:
+(1)男性,1年以上相关工作经验;
+(2)具有独立工作和解决问题的能力...</dd>
+						<dd class="bb" style="height:20px;">
+					<!-- <dd class='bb'>已有<b>23</b>人报名 --><u><a href="/plus/view.php?aid=746">我想去</a></u></dd>
+
+			</dl> 
+            </div>            
+            
+        </div>
+        
+        
+    </div>
+</div>
+    
+<!--            列表                  -->    
+
+
+
+<!-------baoming------->   
+<div class="extra">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-6 col-sm-3">
+				<h5>
+					用户帮助
+				</h5>
+				<ul class="icons-list">
+					
+					<li>
+						<i class="icon-li fa fa-angle-double-right">
+						</i>
+						<a href="#">
+							帐户状态
+						</a>
+					</li>
+					<li>
+						<i class="icon-li fa fa-angle-double-right">
+						</i>
+						<a href="#">
+							密码找回与修改
+						</a>
+					</li>
+				</ul>
+			</div>
+			<!-- /span3 -->
+			<div class="col-xs-6 col-sm-3">
+				<h5>
+					关注我们
+				</h5>
+				<ul class="icons-list">
+					<li>
+						<i class="icon-li fa fa-angle-double-right">
+						</i>
+						<a href="/home/weixin_x.php">
+							微信
+						</a>
+					</li>
+					<li>
+						<i class="icon-li fa fa-angle-double-right">
+						</i>
+						<a href="http://www.1dagong.com/m/index.html">
+							手机客户端
+						</a>
+					</li>
+					<li>
+						<i class="icon-li fa fa-angle-double-right">
+						</i>
+						<a href="http://www.1dagong.com/">
+							手机网页版
+						</a>
+					</li>
+					<li>
+						<i class="icon-li fa fa-angle-double-right">
+						</i>
+						<a href="#">
+							新浪微博
+						</a>
+					</li>
+				</ul>
+			</div>
+			<!-- /span3 -->
+			<div class="col-xs-12 col-sm-3">
+				<h5>
+					壹打工网相关
+				</h5>
+				<ul class="icons-list">
+					<li>
+						<i class="icon-li fa fa-twitter">
+						</i>
+						<a href="#" target="_blank">
+							友情链接
+						</a>
+					</li>
+				</ul>
+			</div>
+			<!-- /span3 -->
+			<div class="col-xs-12 col-sm-3">
+				<h5>
+					免费报名
+				</h5>
+				<p>
+					仅需留下您的手机或电话，我们专业的求职顾问便会迅速为您量身定制合适的职位。
+				</p>
+				<div id="mc_embed_signup">
+					<form class="form-signin" role="form" method="post" action="/home/p_register.php" id="regUser">
+						<input type="hidden" value="regbase" name="dopost"/>
+						<input type="hidden" value="1" name="step"/>
+						<input type="hidden" value="个人" name="mtype"/>
+
+						<div class="mc-field-group form-group">
+							<input type="text" class="required email form-control" placeholder="手机号码" required name="shouye" id="userid" maxlength="13">
+							<datalist id="userid">
+								<option value="0551-12345678">
+								<option value="13912345678">
+								<option value="055112345678">
+							</datalist>
+
+						</div>
+						
+						<div class="clear">
+							<button class="btn btn-primary btn-block" type="submit">免费报名</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+		<!-- /row -->
+	</div>
+	<!-- /container -->
+</div>
+<!--------baoming------->
+
+
+<!-- /footer -->
+<div class="footer">
+	<div class="container">
+		<ul class="nav footer-nav pull-left">
+			<li>
+				© 2013-2015 Www.1dagong.Com 本站所有招聘信息及作品，未经书面授权不得转载！<br />
+				<a href="/help/aboutus.html">关于我们</a>
+			</li>
+		</ul>
+		<ul class="nav footer-nav pull-right">
+			<li>
+				最专业的求职招聘网站，致力打造人才网第一品牌！
+				<i class="fa fa-heart text-primary">
+				</i>
+				&amp;
+				<a href="http://www.1dagong.com" target="_blank">
+					壹打工网.
+				</a><br />
+				国家工业和信息化部备案号：<a href="http://www.miibeian.gov.cn/">皖ICP备13015030号-3</a>
+			</li>
+		</ul>
+	</div>
+	<!-- /container -->
+</div>
+<div class="none">
+<script type="text/javascript">
+var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3Fb41da31689d0e576af45aaeafc3be8d8' type='text/javascript'%3E%3C/script%3E"));
+</script>
+</div>
+<!-- /footer -->
+
+
+</body>
+</html>
